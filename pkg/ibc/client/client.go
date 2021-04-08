776b798d70b6c2342dcdac7e4bdae9d56f77fbb3
Jun Kimura <junkxdev@gmail.com>
2021-04-08 11:31:20 +0900

    add some comments

--- pkg/ibc/client/client.go
+++ pkg/ibc/client/client.go
@@ -2,6 +2,8 @@ package client
 
 // client type
 const (
+	// IBFT2 Client
 	BesuIBFT2Client = "ibft2"
-	MockClient      = "mock"
+	// NOTE: The mock client is only intended for use in development such as ganache.
+	MockClient = "mock"
 )
