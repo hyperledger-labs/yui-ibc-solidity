b21eae91a3df3a13172aa2ce24a0b505d870bf0a
Masanori Yoshida <masanori.yoshida@datachain.jp>
2022-02-19 02:31:40 +0900

    use a new solidity-protobuf file-level option ".solidity.file_options.location"
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- scripts/solpb.sh
+++ scripts/solpb.sh
@@ -6,10 +6,14 @@ if [ -z "$SOLPB_DIR" ]; then
     exit 1
 fi
 
+GEN_DIR="$(pwd)/generated"
+mkdir $(pwd)/generated
 for file in $(find ./proto -name '*.proto')
 do
   echo "Generating "$file
-  protoc -I$(pwd)/proto -I "third_party/proto" -I${SOLPB_DIR}/protobuf-solidity/src/protoc/include --plugin=protoc-gen-sol=${SOLPB_DIR}/protobuf-solidity/src/protoc/plugin/gen_sol.py --"sol_out=gen_runtime=contracts/core/types/ProtoBufRuntime.sol&solc_version=0.8.9&ignore_protos=gogoproto/gogo.proto:$(pwd)" $(pwd)/$file
+  protoc -I$(pwd)/proto -I "third_party/proto" -I${SOLPB_DIR}/protobuf-solidity/src/protoc/include --plugin=protoc-gen-sol=${SOLPB_DIR}/protobuf-solidity/src/protoc/plugin/gen_sol.py --"sol_out=gen_runtime=@hyperledger-labs/yui-ibc-solidity/contracts/core/types/ProtoBufRuntime.sol&solc_version=0.8.9&ignore_protos=gogoproto/gogo.proto:$GEN_DIR" $(pwd)/$file
 done
+cp -r "$GEN_DIR/@hyperledger-labs/yui-ibc-solidity/contracts" "$(pwd)"
+rm -rf "$GEN_DIR"
 
 # npx truffle compile
