954086bcc7edc406917667e664213e47536f76bf
Jun Kimura <jun.kimura@datachain.jp>
2022-12-18 17:39:01 +0900

    migrations: fix deploy script
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,26 +1,24 @@
-const IBFT2Client = artifacts.require("IBFT2Client");
-const MockClient = artifacts.require("MockClient");
 const IBCClient = artifacts.require("IBCClient");
 const IBCConnection = artifacts.require("IBCConnection");
-const IBCChannel = artifacts.require("IBCChannel");
-const IBCHandler = artifacts.require("OwnableIBCHandler");
-const IBCMsgs = artifacts.require("IBCMsgs");
+const IBCChannelHandshake = artifacts.require("IBCChannelHandshake");
+const IBCPacket = artifacts.require("IBCPacket");
 const IBCCommitment = artifacts.require("IBCCommitment");
+const IBCHandler = artifacts.require("OwnableIBCHandler");
 const SimpleToken = artifacts.require("SimpleToken");
 const ICS20TransferBank = artifacts.require("ICS20TransferBank");
 const ICS20Bank = artifacts.require("ICS20Bank");
+const IBFT2Client = artifacts.require("IBFT2Client");
+const MockClient = artifacts.require("MockClient");
 
 module.exports = async function (deployer) {
   await deployer.deploy(IBCCommitment);
-  await deployer.link(IBCCommitment, [IBCHandler, IBCClient, IBCConnection, IBCChannel]);
-
-  await deployer.deploy(IBCMsgs);
-  await deployer.link(IBCMsgs, [IBCClient, IBCConnection, IBCChannel, IBCHandler]);
+  await deployer.link(IBCCommitment, [IBCHandler, IBCClient, IBCConnection, IBCChannelHandshake, IBCPacket]);
 
   await deployer.deploy(IBCClient);
   await deployer.deploy(IBCConnection);
-  await deployer.deploy(IBCChannel);
-  await deployer.deploy(IBCHandler, IBCClient.address, IBCConnection.address, IBCChannel.address, IBCChannel.address);
+  await deployer.deploy(IBCChannelHandshake);
+  await deployer.deploy(IBCPacket);
+  await deployer.deploy(IBCHandler, IBCClient.address, IBCConnection.address, IBCChannelHandshake.address, IBCPacket.address);
 
   await deployer.deploy(MockClient, IBCHandler.address);
   await deployer.deploy(IBFT2Client, IBCHandler.address);
