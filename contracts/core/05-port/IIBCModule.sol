f43337f67cb7c6ff1accae6579933182ee31d325
Jun Kimura <jun.kimura@datachain.jp>
2023-10-13 23:36:36 +0900

    use struct type as callback parameters of channel handshake
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- contracts/core/05-port/IIBCModule.sol
+++ contracts/core/05-port/IIBCModule.sol
@@ -6,62 +6,109 @@ import "../../proto/Channel.sol";
 // IIBCModule defines an interface that implements all the callbacks
 // that modules must define as specified in ICS-26
 interface IIBCModule {
-    // onChanOpenInit will verify that the relayer-chosen parameters
-    // are valid and perform any custom INIT logic.
-    // It may return an error if the chosen parameters are invalid
-    // in which case the handshake is aborted.
-    // If the provided version string is non-empty, OnChanOpenInit should return
-    // the version string if valid or an error if the provided version is invalid.
-    // If the version string is empty, OnChanOpenInit is expected to
-    // return a default version string representing the version(s) it supports.
-    // If there is no default version string for the application,
-    // it should return an error if provided version is empty string.
-    function onChanOpenInit(
-        Channel.Order,
-        string[] calldata connectionHops,
-        string calldata portId,
-        string calldata channelId,
-        ChannelCounterparty.Data calldata counterparty,
-        string calldata version
-    ) external returns (string memory);
+    struct MsgOnChanOpenInit {
+        Channel.Order order;
+        string[] connectionHops;
+        string portId;
+        string channelId;
+        ChannelCounterparty.Data counterparty;
+        string version;
+    }
 
-    // OnChanOpenTry will verify the relayer-chosen parameters along with the
-    // counterparty-chosen version string and perform custom TRY logic.
-    // If the relayer-chosen parameters are invalid, the callback must return
-    // an error to abort the handshake. If the counterparty-chosen version is not
-    // compatible with this modules supported versions, the callback must return
-    // an error to abort the handshake. If the versions are compatible, the try callback
-    // must select the final version string and return it to core IBC.
-    // OnChanOpenTry may also perform custom initialization logic
-    function onChanOpenTry(
-        Channel.Order,
-        string[] calldata connectionHops,
-        string calldata portId,
-        string calldata channelId,
-        ChannelCounterparty.Data calldata counterparty,
-        string calldata counterpartyVersion
-    ) external returns (string memory);
+    struct MsgOnChanOpenTry {
+        Channel.Order order;
+        string[] connectionHops;
+        string portId;
+        string channelId;
+        ChannelCounterparty.Data counterparty;
+        string counterpartyVersion;
+    }
 
-    // OnChanOpenAck will error if the counterparty selected version string
-    // is invalid to abort the handshake. It may also perform custom ACK logic.
-    function onChanOpenAck(string calldata portId, string calldata channelId, string calldata counterpartyVersion)
-        external;
+    struct MsgOnChanOpenAck {
+        string portId;
+        string channelId;
+        string counterpartyVersion;
+    }
 
-    // OnChanOpenConfirm will perform custom CONFIRM logic and may error to abort the handshake.
-    function onChanOpenConfirm(string calldata portId, string calldata channelId) external;
+    struct MsgOnChanOpenConfirm {
+        string portId;
+        string channelId;
+    }
 
-    function onChanCloseInit(string calldata portId, string calldata channelId) external;
+    struct MsgOnChanCloseInit {
+        string portId;
+        string channelId;
+    }
 
-    function onChanCloseConfirm(string calldata portId, string calldata channelId) external;
+    struct MsgOnChanCloseConfirm {
+        string portId;
+        string channelId;
+    }
 
-    // OnRecvPacket must return an acknowledgement that implements the Acknowledgement interface.
-    // In the case of an asynchronous acknowledgement, nil should be returned.
-    // If the acknowledgement returned is successful, the state changes on callback are written,
-    // otherwise the application state changes are discarded. In either case the packet is received
-    // and the acknowledgement is written (in synchronous cases).
+    /**
+     * @dev onChanOpenInit will verify that the relayer-chosen parameters
+     * are valid and perform any custom INIT logic.
+     * It may return an error if the chosen parameters are invalid
+     * in which case the handshake is aborted.
+     * If the provided version string is non-empty, OnChanOpenInit should return
+     * the version string if valid or an error if the provided version is invalid.
+     * If the version string is empty, OnChanOpenInit is expected to
+     * return a default version string representing the version(s) it supports.
+     * If there is no default version string for the application,
+     * it should return an error if provided version is empty string.
+     */
+    function onChanOpenInit(MsgOnChanOpenInit calldata msg_) external returns (string memory);
+
+    /**
+     * @dev onChanOpenTry will verify the relayer-chosen parameters along with the
+     * counterparty-chosen version string and perform custom TRY logic.
+     * If the relayer-chosen parameters are invalid, the callback must return
+     * an error to abort the handshake. If the counterparty-chosen version is not
+     * compatible with this modules supported versions, the callback must return
+     * an error to abort the handshake. If the versions are compatible, the try callback
+     * must select the final version string and return it to core IBC.
+     * OnChanOpenTry may also perform custom initialization logic
+     */
+    function onChanOpenTry(MsgOnChanOpenTry calldata msg_) external returns (string memory);
+
+    /**
+     * @dev OnChanOpenAck will error if the counterparty selected version string
+     * is invalid to abort the handshake. It may also perform custom ACK logic.
+     */
+    function onChanOpenAck(MsgOnChanOpenAck calldata msd_) external;
+
+    /**
+     * @dev OnChanOpenConfirm will perform custom CONFIRM logic and may error to abort the handshake.
+     */
+    function onChanOpenConfirm(MsgOnChanOpenConfirm calldata msg_) external;
+
+    /**
+     * @dev OnChanCloseInit will perform custom CLOSE_INIT logic and may error to abort the handshake.
+     * NOTE: If the application does not allow the channel to be closed, this function must revert.
+     */
+    function onChanCloseInit(MsgOnChanCloseInit calldata msg_) external;
+
+    /**
+     * @dev OnChanCloseConfirm will perform custom CLOSE_CONFIRM logic and may error to abort the handshake.
+     */
+    function onChanCloseConfirm(MsgOnChanCloseConfirm calldata msg_) external;
+
+    /**
+     * @dev OnRecvPacket must return an acknowledgement that implements the Acknowledgement interface.
+     * In the case of an asynchronous acknowledgement, nil should be returned.
+     * If the acknowledgement returned is successful, the state changes on callback are written,
+     * otherwise the application state changes are discarded. In either case the packet is received
+     * and the acknowledgement is written (in synchronous cases).
+     */
     function onRecvPacket(Packet.Data calldata, address relayer) external returns (bytes memory);
 
+    /**
+     * @dev onAcknowledgementPacket is called when a packet sent by this module has been acknowledged.
+     */
     function onAcknowledgementPacket(Packet.Data calldata, bytes calldata acknowledgement, address relayer) external;
 
+    /**
+     * @dev onTimeoutPacket is called when a packet sent by this module has timed-out (such that it will not be received on the destination chain).
+     */
     function onTimeoutPacket(Packet.Data calldata, address relayer) external;
 }
