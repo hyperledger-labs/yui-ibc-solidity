e697cddd4618b3989a6db660501349aee51d9165
Jun Kimura <jun.kimura@datachain.jp>
2023-10-20 09:40:45 +0900

    add more test cases
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- tests/foundry/src/ICS04.t.sol
+++ tests/foundry/src/ICS04.t.sol
@@ -61,6 +61,7 @@ abstract contract TestICS04Helper is TestIBCBase, TestMockClientHelper {
         ChannelInfo memory counterpartyChannelInfo,
         Height.Data memory proofHeight
     ) internal pure returns (IBCMsgs.MsgChannelOpenTry memory) {
+        assert(bytes(channelInfo.channelId).length == 0 && bytes(channelInfo.version).length == 0);
         bytes memory proofInit = genMockChannelStateProof(
             proofHeight,
             counterpartyChannelInfo.portId,
@@ -83,7 +84,7 @@ abstract contract TestICS04Helper is TestIBCBase, TestMockClientHelper {
                     channel_id: counterpartyChannelInfo.channelId
                 }),
                 connection_hops: newConnectionHops(channelInfo.connectionId),
-                version: channelInfo.version
+                version: ""
             }),
             counterpartyVersion: counterpartyChannelInfo.version,
             proofInit: proofInit,
@@ -146,6 +147,38 @@ abstract contract TestICS04Helper is TestIBCBase, TestMockClientHelper {
         connectionHops[0] = connectionId;
         return connectionHops;
     }
+
+    function validatePostStateAfterChanOpenInit(
+        IBCMsgs.MsgChannelOpenInit memory msg_,
+        Channel.Data memory channel,
+        string memory version
+    ) internal {
+        assertTrue(channel.state == Channel.State.STATE_INIT);
+        assertTrue(channel.ordering == msg_.channel.ordering);
+        assertEq(channel.counterparty.port_id, msg_.channel.counterparty.port_id);
+        assertEq(channel.counterparty.channel_id, "");
+        assertEq(channel.connection_hops.length, msg_.channel.connection_hops.length);
+        for (uint256 i = 0; i < channel.connection_hops.length; i++) {
+            assertEq(channel.connection_hops[i], msg_.channel.connection_hops[i]);
+        }
+        assertEq(channel.version, version);
+    }
+
+    function validatePostStateAfterChanOpenTry(
+        IBCMsgs.MsgChannelOpenTry memory msg_,
+        Channel.Data memory channel,
+        string memory version
+    ) internal {
+        assertTrue(channel.state == Channel.State.STATE_TRYOPEN);
+        assertTrue(channel.ordering == msg_.channel.ordering);
+        assertEq(channel.counterparty.port_id, msg_.channel.counterparty.port_id);
+        assertEq(channel.counterparty.channel_id, msg_.channel.counterparty.channel_id);
+        assertEq(channel.connection_hops.length, msg_.channel.connection_hops.length);
+        for (uint256 i = 0; i < channel.connection_hops.length; i++) {
+            assertEq(channel.connection_hops[i], msg_.channel.connection_hops[i]);
+        }
+        assertEq(channel.version, version);
+    }
 }
 
 contract TestICS04Handshake is TestIBCBase, TestMockClientHelper, TestICS03Helper, TestICS04Helper {
@@ -191,34 +224,34 @@ contract TestICS04Handshake is TestIBCBase, TestMockClientHelper, TestICS03Helpe
         );
 
         {
-            (string memory channelId, string memory version) = handler.channelOpenInit(
-                msgChannelOpenInit(
-                    ChannelInfo("portidone", "", Channel.Order.ORDER_ORDERED, MOCK_APP_VERSION, connectionId),
-                    "portidtwo"
-                )
+            IBCMsgs.MsgChannelOpenInit memory msg_ = msgChannelOpenInit(
+                ChannelInfo("portidone", "", Channel.Order.ORDER_ORDERED, MOCK_APP_VERSION, connectionId), "portidtwo"
             );
+            (string memory channelId, string memory version) = handler.channelOpenInit(msg_);
             assertEq(channelId, "channel-0");
             assertEq(version, MOCK_APP_VERSION);
+            (Channel.Data memory channel,) = handler.getChannel("portidone", channelId);
+            validatePostStateAfterChanOpenInit(msg_, channel, MOCK_APP_VERSION);
         }
         {
-            (string memory channelId, string memory version) = handler.channelOpenInit(
-                msgChannelOpenInit(
-                    ChannelInfo("portidone", "", Channel.Order.ORDER_ORDERED, MOCK_APP_VERSION, connectionId),
-                    "portidtwo"
-                )
+            IBCMsgs.MsgChannelOpenInit memory msg_ = msgChannelOpenInit(
+                ChannelInfo("portidone", "", Channel.Order.ORDER_ORDERED, MOCK_APP_VERSION, connectionId), "portidtwo"
             );
+            (string memory channelId, string memory version) = handler.channelOpenInit(msg_);
             assertEq(channelId, "channel-1");
             assertEq(version, MOCK_APP_VERSION);
+            (Channel.Data memory channel,) = handler.getChannel("portidone", channelId);
+            validatePostStateAfterChanOpenInit(msg_, channel, MOCK_APP_VERSION);
         }
         {
-            (string memory channelId, string memory version) = handler.channelOpenInit(
-                msgChannelOpenInit(
-                    ChannelInfo("portidone", "", Channel.Order.ORDER_UNORDERED, MOCK_APP_VERSION, connectionId),
-                    "portidtwo"
-                )
+            IBCMsgs.MsgChannelOpenInit memory msg_ = msgChannelOpenInit(
+                ChannelInfo("portidone", "", Channel.Order.ORDER_UNORDERED, MOCK_APP_VERSION, connectionId), "portidtwo"
             );
+            (string memory channelId, string memory version) = handler.channelOpenInit(msg_);
             assertEq(channelId, "channel-2");
             assertEq(version, MOCK_APP_VERSION);
+            (Channel.Data memory channel,) = handler.getChannel("portidone", channelId);
+            validatePostStateAfterChanOpenInit(msg_, channel, MOCK_APP_VERSION);
         }
     }
 
@@ -330,50 +363,185 @@ contract TestICS04Handshake is TestIBCBase, TestMockClientHelper, TestICS03Helpe
         );
 
         {
-            (string memory channelId, string memory version) = handler.channelOpenTry(
-                msgChannelOpenTry(
-                    ChannelInfo({
-                        portId: "portidone",
-                        channelId: "",
-                        ordering: Channel.Order.ORDER_ORDERED,
-                        version: MOCK_APP_VERSION,
-                        connectionId: connectionId
-                    }),
-                    ChannelInfo({
-                        portId: "portidtwo",
-                        channelId: "channel-0",
-                        ordering: Channel.Order.ORDER_ORDERED,
-                        version: MOCK_APP_VERSION,
-                        connectionId: counterpartyConnectionId
-                    }),
-                    H(0, 1)
-                )
+            IBCMsgs.MsgChannelOpenTry memory msg_ = msgChannelOpenTry(
+                ChannelInfo({
+                    portId: "portidone",
+                    channelId: "",
+                    ordering: Channel.Order.ORDER_ORDERED,
+                    version: "",
+                    connectionId: connectionId
+                }),
+                ChannelInfo({
+                    portId: "portidtwo",
+                    channelId: "channel-0",
+                    ordering: Channel.Order.ORDER_ORDERED,
+                    version: MOCK_APP_VERSION,
+                    connectionId: counterpartyConnectionId
+                }),
+                H(0, 1)
             );
+            (string memory channelId, string memory version) = handler.channelOpenTry(msg_);
             assertEq(channelId, "channel-0");
             assertEq(version, MOCK_APP_VERSION);
+            (Channel.Data memory channel,) = handler.getChannel("portidone", channelId);
+            validatePostStateAfterChanOpenTry(msg_, channel, MOCK_APP_VERSION);
         }
         {
-            (string memory channelId, string memory version) = handler.channelOpenTry(
-                msgChannelOpenTry(
-                    ChannelInfo({
-                        portId: "portidone",
-                        channelId: "",
-                        ordering: Channel.Order.ORDER_UNORDERED,
-                        version: MOCK_APP_VERSION,
-                        connectionId: connectionId
-                    }),
-                    ChannelInfo({
-                        portId: "portidtwo",
-                        channelId: "channel-0",
-                        ordering: Channel.Order.ORDER_UNORDERED,
-                        version: MOCK_APP_VERSION,
-                        connectionId: counterpartyConnectionId
-                    }),
-                    H(0, 1)
-                )
+            IBCMsgs.MsgChannelOpenTry memory msg_ = msgChannelOpenTry(
+                ChannelInfo({
+                    portId: "portidone",
+                    channelId: "",
+                    ordering: Channel.Order.ORDER_UNORDERED,
+                    version: "",
+                    connectionId: connectionId
+                }),
+                ChannelInfo({
+                    portId: "portidtwo",
+                    channelId: "channel-0",
+                    ordering: Channel.Order.ORDER_UNORDERED,
+                    version: MOCK_APP_VERSION,
+                    connectionId: counterpartyConnectionId
+                }),
+                H(0, 1)
             );
+            (string memory channelId, string memory version) = handler.channelOpenTry(msg_);
             assertEq(channelId, "channel-1");
             assertEq(version, MOCK_APP_VERSION);
+            (Channel.Data memory channel,) = handler.getChannel("portidone", channelId);
+            validatePostStateAfterChanOpenTry(msg_, channel, MOCK_APP_VERSION);
+        }
+    }
+
+    function testInvalidChanOpenTry() public {
+        string memory clientId = createMockClient(handler, 1);
+        string memory counterpartyClientId = createMockClient(counterpartyHandler, 1, 2);
+        string memory connectionId = genConnectionId(0);
+        string memory counterpartyConnectionId = genConnectionId(1);
+
+        Version.Data memory orderedVersion = orderedIBCVersion();
+
+        // connection does not exist
+        {
+            IBCMsgs.MsgChannelOpenTry memory msg_ = msgChannelOpenTry(
+                ChannelInfo({
+                    portId: "portidone",
+                    channelId: "",
+                    ordering: Channel.Order.ORDER_ORDERED,
+                    version: "",
+                    connectionId: connectionId
+                }),
+                ChannelInfo({
+                    portId: "portidtwo",
+                    channelId: "channel-0",
+                    ordering: Channel.Order.ORDER_ORDERED,
+                    version: MOCK_APP_VERSION,
+                    connectionId: counterpartyConnectionId
+                }),
+                H(0, 1)
+            );
+            vm.expectRevert();
+            handler.channelOpenTry(msg_);
+        }
+
+        {
+            setConnection(
+                handler, clientId, connectionId, counterpartyClientId, counterpartyConnectionId, orderedIBCVersion()
+            );
+            IBCMsgs.MsgChannelOpenTry memory msg_ = msgChannelOpenTry(
+                ChannelInfo({
+                    portId: "portidone",
+                    channelId: "",
+                    ordering: Channel.Order.ORDER_UNORDERED,
+                    version: "",
+                    connectionId: connectionId
+                }),
+                ChannelInfo({
+                    portId: "portidtwo",
+                    channelId: "channel-0",
+                    ordering: Channel.Order.ORDER_UNORDERED,
+                    version: MOCK_APP_VERSION,
+                    connectionId: counterpartyConnectionId
+                }),
+                H(0, 1)
+            );
+            vm.expectRevert();
+            handler.channelOpenTry(msg_);
+        }
+
+        // invalid proof height
+        {
+            IBCMsgs.MsgChannelOpenTry memory msg_ = msgChannelOpenTry(
+                ChannelInfo({
+                    portId: "portidone",
+                    channelId: "",
+                    ordering: Channel.Order.ORDER_ORDERED,
+                    version: "",
+                    connectionId: connectionId
+                }),
+                ChannelInfo({
+                    portId: "portidtwo",
+                    channelId: "channel-0",
+                    ordering: Channel.Order.ORDER_ORDERED,
+                    version: MOCK_APP_VERSION,
+                    connectionId: counterpartyConnectionId
+                }),
+                H(0, 2)
+            );
+            vm.expectRevert();
+            handler.channelOpenTry(msg_);
+        }
+
+        // not binded port
+        {
+            setConnection(
+                handler, clientId, connectionId, counterpartyClientId, counterpartyConnectionId, orderedVersion
+            );
+            IBCMsgs.MsgChannelOpenTry memory msg_ = msgChannelOpenTry(
+                ChannelInfo({
+                    portId: "portidthree",
+                    channelId: "",
+                    ordering: Channel.Order.ORDER_ORDERED,
+                    version: "",
+                    connectionId: connectionId
+                }),
+                ChannelInfo({
+                    portId: "portidtwo",
+                    channelId: "channel-0",
+                    ordering: Channel.Order.ORDER_ORDERED,
+                    version: MOCK_APP_VERSION,
+                    connectionId: counterpartyConnectionId
+                }),
+                H(0, 1)
+            );
+            vm.expectRevert();
+            handler.channelOpenTry(msg_);
+        }
+
+        // app version mismatch between proof and msg
+        {
+            setConnection(
+                handler, clientId, connectionId, counterpartyClientId, counterpartyConnectionId, orderedVersion
+            );
+            IBCMsgs.MsgChannelOpenTry memory msg_ = msgChannelOpenTry(
+                ChannelInfo({
+                    portId: "portidone",
+                    channelId: "",
+                    ordering: Channel.Order.ORDER_ORDERED,
+                    version: "",
+                    connectionId: connectionId
+                }),
+                ChannelInfo({
+                    portId: "portidtwo",
+                    channelId: "channel-0",
+                    ordering: Channel.Order.ORDER_ORDERED,
+                    version: MOCK_APP_VERSION,
+                    connectionId: counterpartyConnectionId
+                }),
+                H(0, 1)
+            );
+            msg_.counterpartyVersion = "mockapp-2";
+            vm.expectRevert();
+            handler.channelOpenTry(msg_);
         }
     }
 }
