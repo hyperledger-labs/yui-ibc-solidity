ff57c65fbc7653d74fae022d1f5d8d0c513c5e57
Jun Kimura <jun.kimura@datachain.jp>
2024-02-22 14:40:46 +0900

    fix test
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -14,7 +14,7 @@ func TestRevertReasonParser(t *testing.T) {
 		hexToBytes("0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000001a4e6f7420656e6f7567682045746865722070726f76696465642e000000000000"),
 	)
 	require.NoError(t, err)
-	require.Equal(t, "Error(string)", s)
+	require.Equal(t, "Error(string)", s.Sig)
 	require.Equal(t, []interface{}{"Not enough Ether provided."}, args)
 }
 
